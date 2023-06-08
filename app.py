7c6ccd20906daafc02f9d124b9ddd53c4427586d
ntua1007 <132580168+ntua1007@users.noreply.github.com>
2023-06-09 00:00:51 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import *
 import os
-import random
+import random 
 import openai
 
 app = Flask(__name__)
