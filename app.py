59fda0a5dd88a87620c58b4af308df9766ce00ec
ntua1007 <132580168+ntua1007@users.noreply.github.com>
2023-06-14 22:19:29 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,14 +4,11 @@ from linebot.exceptions import InvalidSignatureError
 from linebot.models import *
 import os
 import random
-import openai
-
 
 app = Flask(__name__)
 
 line_bot_api = LineBotApi(os.environ['CHANNEL_ACCESS_TOKEN'])
 handler = WebhookHandler(os.environ['CHANNEL_SECRET'])
-openai.api_key = os.environ['OPENAI_SECRET']
 
 cats_N = ['橘貓', '黑貓', '白貓', '藍貓', '奶油貓', '三花貓', '玳瑁貓', '賓士貓', '乳牛貓', '灰虎斑貓', '棕虎斑貓', '三花虎斑貓', '白底灰虎斑貓', '白底棕虎斑貓', '白底橘虎斑貓', '黑底白襪貓', '橘底白襪貓', '灰底白襪貓', '棕底白襪貓', '啵啵貓', '尷尬貓', '哭哭貓', '無耳貓', '九命貓', '豹貓', '暹羅貓', '布偶貓', '無毛貓', '波斯貓', '緬因貓', '美國捲耳貓', '挪威森林貓', '狸貓', '熊貓']
 cats_R = ['薄荷冰淇淋貓', '草莓蛋糕貓', '香蕉巧克力貓', '外星幽浮貓', '藍莓優格貓', '柳橙果醬貓', '戀愛棉花糖貓', '宇治抹茶貓', '櫻桃馬卡龍貓', '檸檬汽水貓']
@@ -170,20 +167,6 @@ def callback():
 
 def handle_message(event):
     
-        ai_msg = event.message.text[:6].lower()
-    if ai_msg == 'hi ai:':
-        # 將第六個字元之後的訊息發送給 OpenAI
-        response = openai.Completion.create(
-            model='text-davinci-003',
-            prompt=event.message.text[6:],
-            max_tokens=256,
-            temperature=0.5,
-        )
-        # 接收到回覆訊息後，移除換行符號
-        reply_msg = TextSendMessage(text=response["choices"][0]["text"].replace('\n',''))
-        # 使用 Line Bot API 回覆訊息給使用者
-        line_bot_api.reply_message(event.reply_token, reply_msg)
-
     if event.message.text == '找貓咪':
         cat_name, cat_rarity, cat_action = generate_random_cat()
         cat_card = generate_cat_card(cat_name, cat_rarity, cat_action)
