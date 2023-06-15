a9edce39c8c05acedb0d96e2aa160c437a3d1266
ntua1007 <132580168+ntua1007@users.noreply.github.com>
2023-06-15 15:23:29 +0800

    Update app.py

--- app.py
+++ app.py
@@ -237,6 +237,14 @@ def cat_fortune_telling():
 # 分析收到的訊息類型
 @app.route("/callback", methods=['POST'])
 def callback():
+    signature = request.headers['X-Line-Signature']
+    body = request.get_data(as_text=True)
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+    return 'OK'
+
     # 获取请求中的事件
     events = request.json['events']
     for event in events:
@@ -259,7 +267,6 @@ def callback():
                     return 'OK'
     return 'OK'
 
-
     
 #貓貓運勢結束
 
@@ -267,15 +274,6 @@ def callback():
 def index():
     return 'Hello World!'
 
-@app.route("/callback", methods=['POST'])
-def callback():
-    signature = request.headers['X-Line-Signature']
-    body = request.get_data(as_text=True)
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400)
-    return 'OK'
 
 @handler.add(MessageEvent, message=TextMessage)
 
